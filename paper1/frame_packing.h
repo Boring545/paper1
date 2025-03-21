77658ce2b31e2c8fd11bbb288ba91cacc68ccfed
jinqi zhang <jasonbus@163.com>
2025-03-21 21:13:19 +0800

    01

--- paper1/frame_packing.h
+++ paper1/frame_packing.h
@@ -6,22 +6,25 @@
 namespace cfd {
 
 	struct ComparatorIndexByValue {
-		const MessageVec& vec;  
-
+		const MessageVec& mvec;  
+		const std::vector<int>& vec;
 		// 构造函数，传入数组指针
-		ComparatorIndexByValue(const MessageVec& v) : vec(v) {}
+		ComparatorIndexByValue(const MessageVec& mv, const std::vector<int>& v) : mvec(mv),vec(v) {}
 
 		bool operator()(int idx1, int idx2) const {
-			return vec[idx1].get_offset() < vec[idx2].get_offset();  // 根据 offset 排序
+			return mvec[vec[idx1]].get_offset() < mvec[vec[idx2]].get_offset();  // 根据 offset 排序
 		}
 	};
 
 
 	//一个打包方案，包含一组装载了全部消息的CANFD帧
 	class PackingScheme {
-		using PeriodMessageMap = std::map<int, MessageVec>;	// 相同ECUpair下，不同周期对应的message索引分表
-		using EcuToFrameMap = std::unordered_map<EcuPair, std::vector<int>>;	// CANFD帧分表（哈希表）：键是源ECU和目的ECU的组合，值是对应的frame的数组索引
-		using EcuPeriodMessageMap = std::unordered_map<EcuPair, PeriodMessageMap, EcuPairHash>;	// message按照ECU对、period的二级分表
+		using PeriodFrameeMap = std::map<int, std::vector<int>>;		// 相同ECUpair下，不同周期对应的frame索引分表,通过frame_set[索引]选取frame
+		using EcuToFrameMap = std::unordered_map<EcuPair, PeriodFrameeMap, EcuPairHash>;	// 根据【ECUpair，period】选择PeriodFrameeMap分表
+
+		using PeriodMessageMap = std::map<int, std::vector<int>>;		// 相同ECUpair下，不同周期对应的message索引分表,通过message_set[索引]选取message
+		using EcuPeriodMessageMap = std::unordered_map<EcuPair, PeriodMessageMap, EcuPairHash>;	// 根据【ECUpair，period】选择PeriodMessageMap分表
+
 
 	private:
 		MessageVec message_set;
@@ -46,13 +49,13 @@ namespace cfd {
 				message_set.emplace_back(i);
 				period_msg_map[vec[i].ecu_pair][vec[i].period].emplace_back(i);// 按ecu对和周期将消息分组
 			}
-			initial_frames();
+			//initial_frames();
 
 		}
 		// 根据消息集合生成一个初始帧列表，TODO 生成一张ECU对的分表
 		void initial_frames() {
 			int period = 0;
-			int frame_id = 0;
+			FrameId frame_id = 0;
 
 			//每次遍历处理一对ecu的单向传输
 			for (auto& ecu_map : period_msg_map) {
@@ -60,7 +63,7 @@ namespace cfd {
 				for (auto& period_map : ecu_map.second) {
 					period = period_map.first;
 					//索引set，存储索引，按照 Message 的 offset 排序
-					std::set<int, ComparatorIndexByValue> temp_set(ComparatorIndexByValue(period_map.second));
+					std::set<int, ComparatorIndexByValue> temp_set(ComparatorIndexByValue(message_set,period_map.second));
 					// 插入索引
 					for (int i = 0; i < period_map.second.size(); ++i) {
 						temp_set.insert(i);
@@ -70,17 +73,17 @@ namespace cfd {
 					while (mit != temp_set.end()) {
 						auto next_mit = std::next(mit);	//下一个迭代器，为了避免mit删除后失效
 						int frame_index = frame_set.size();	//标记当前正在装载的frame
-						int left = period_map.second[*mit].get_offset();		//当前分析的消息mit对应的有效区间左边界
-						int right = left + period_map.second[*mit].get_deadline() * FACTOR_MSG_WAIT_WINDOW;
+						int left = message_set[period_map.second[*mit]].get_offset();		//当前分析的消息mit对应的有效区间左边界
+						int right = left + message_set[period_map.second[*mit]].get_deadline() * FACTOR_MSG_WAIT_WINDOW;
 
-						frame_set.emplace_back(frame_id++, period_map.second[*mit]);	// 创建一个帧，同时向帧中插入当前消息mit，此操作一定能成功
+						frame_set.emplace_back(frame_id++, message_set[period_map.second[*mit]]);	// 创建一个帧，同时向帧中插入当前消息mit，此操作一定能成功
 						temp_set.erase(mit);											// 删除已经插入的消息mit
 						
 						//向当前分析的帧frame_set[frame_index]反复插入message
-						while (next_mit != temp_set.end() && period_map.second[*next_mit].get_offset() <= right) {
+						while (next_mit != temp_set.end() && message_set[period_map.second[*next_mit]].get_offset() <= right) {
 							mit = next_mit;
 							next_mit = std::next(mit);
-							bool inserted = frame_set[frame_index].add_message(period_map.second[*mit], SIZE_LIMIT_CANFD_DATA);
+							bool inserted = frame_set[frame_index].add_message(message_set[period_map.second[*mit]], SIZE_LIMIT_CANFD_DATA);
 							if (inserted) {
 								// 如果消息成功插入，删除该消息，插入失败则跳过该消息
 								temp_set.erase(mit);
